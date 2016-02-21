6d56b042fb60ce98696c04cbc6d38aeeead41e66
Noam Youngerman <noamyoungerm@gmail.com>
2016-02-21 12:23:11 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var DEFAULT = {base: '/'};
-var base = (this.book.options.pluginsConfig.mathjax || DEFAULT).base;
-
 module.exports = {
   website: {
     html: {
-        "head:start": '<base href="' + base + '">',
+        "head:start": '<base href="' + this.options.base || '/' + '">',
     }
   }
 };
