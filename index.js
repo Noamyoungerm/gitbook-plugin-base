03c0784cb4586692a9eeed9c4a5c1eda9a74037b
Noam Youngerman <noamyoungerm@gmail.com>
2016-02-21 14:57:32 +0200

    minor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   website: {
     html: {
       "head:start": function() {
-        console.log(this.book.config);
+        console.log(this.book.config.options);
         var util = require('util');
         var str = util.format(this.book.config.pluginsConfig);
         return '<base href="' + ('' || '/') + '">' + str;
